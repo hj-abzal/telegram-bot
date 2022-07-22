de19ba43d9e7ddfcc4f58d9591b94d2a3642c764
Abzal <abzal008suan@gmail.com>
2022-07-23 01:36:22 +0600

    .

--- src/app/app.js
+++ src/app/app.js
@@ -9,6 +9,7 @@ const optionsCreator = (buttons) => {
     }
 }
 
+// const bot = new TelegramApi('5530425128:AAFBK5i5Xmo_667o3GE5J5gFCgIaOvWjseo', {polling: true}); //for test
 const bot = new TelegramApi('5312862661:AAHPYYDPJ5wwKcrqqR15HRuj7cm3-of8z44', {polling: true});
 
 const data = {
@@ -17,12 +18,29 @@ const data = {
         2: 'https://it-karate.notion.site/2-ef60722032754c10af655f1dbb7fd6af',
         3: 'https://it-karate.notion.site/3-a6f546947c48438ab803d4d6edc400f2',
         4: 'https://it-karate.notion.site/4-d240140034f64f91b904f620828ebcf5',
-        5: 'https://docs.google.com/document/d/1FaJSQELWAI0J4Jv0sfjxxhv2zp_DTgto-h0rWcCgZkA/edit',
-        6: 'https://docs.google.com/document/d/1CMlkobpygyqrFRzUXynMjHXYQsT-ZVT7SvGfOqW2eQk/edit',
-        7: 'https://docs.google.com/document/d/1zqXfG5MRsEFcDTzUuKL4wVNRsvVmI_1NxTRr508jMzM/edit',
+        5: `
+homework: https://docs.google.com/document/d/1FaJSQELWAI0J4Jv0sfjxxhv2zp_DTgto-h0rWcCgZkA/edit
+todolist: https://docs.google.com/document/d/12yGAh5E1Q0u68P8ZZKeGDPb9x7yB5tbOXOpHxy0wk_4/edit?usp=sharing`,
+        6: 'todolist: https://docs.google.com/document/d/1CMlkobpygyqrFRzUXynMjHXYQsT-ZVT7SvGfOqW2eQk/edit     ',
+        7:
+`homework: https://docs.google.com/document/d/1zqXfG5MRsEFcDTzUuKL4wVNRsvVmI_1NxTRr508jMzM/edit
+
+todolist: https://docs.google.com/document/d/11l-d6Hh--Gv8deUnlouvhnK-MEhT0V15aUHjsdImFvE/edit?usp=sharing`,
+        8:
+`homework: https://docs.google.com/document/d/1oc3g1OrtMR-DemqWhF2aJ5cJRL7fhbp-JBXUrwTS2CE/edit?usp=sharing
+
+todolist: https://docs.google.com/document/d/12E8s1HXbiuQvF7t1HcvSUwEJJkKAGbE4PUISvRYvN3I/edit?usp=sharing`,
     },
     2: {
-        1: 'https://docs.google.com/document/d/1n-PnQl6z3ws3R0wALn-LlWj_vJO4fXIhGb61jrwku6A/edit'
+        1:
+`homework: https://docs.google.com/document/d/1n-PnQl6z3ws3R0wALn-LlWj_vJO4fXIhGb61jrwku6A/edit
+
+todolist: https://it-karate.notion.site/Todolist-1-d6fcbc73d0574e5196a1c55f97f7667c`,
+        2:
+`https://docs.google.com/document/d/1zqXfG5MRsEFcDTzUuKL4wVNRsvVmI_1NxTRr508jMzM/edit
+
+todolist: https://it-karate.notion.site/Todolist-2-e208d9d3719e4dc089b3368f183f7fa6 `,
+        3: `homework: https://docs.google.com/document/d/1oc3g1OrtMR-DemqWhF2aJ5cJRL7fhbp-JBXUrwTS2CE/edit?usp=sharing`,
     }
 }
 const students = {
@@ -32,7 +50,7 @@ const students = {
     'ayan_rn': 1,
     'rs6avantc7': 1,
     'rakhymzhan_ff': 1,
-    'SuanAbzal': 2,
+    'SuanAbzal': 1,
     'ugagva': 1,
     'Nakysbek': 2,
     'Aqs0ul': 1
@@ -41,11 +59,13 @@ const students = {
 const lesson1 = [
     [{text: 'Урок 1', callback_data: '1-1'},{text: 'Урок 2', callback_data: '1-2'},{text: 'Урок 3', callback_data: '1-3'}],
     [{text: 'Урок 4', callback_data: '1-4'},{text: 'Урок 5', callback_data: '1-5'},{text: 'Урок 6', callback_data: '1-6'}],
-    [{text: 'Урок 7', callback_data: '1-7'}],
+    [{text: 'Урок 7', callback_data: '1-7'},{text: 'Урок 8', callback_data: '1-8'}],
 ]
 
 const lesson2 = [
     [{text: 'Урок 1', callback_data: '2-1'}],
+    [{text: 'Урок 2', callback_data: '2-2'}],
+    [{text: 'Урок 3', callback_data: '2-3'}],
 ]
 const lessons = {
     1: lesson1,
